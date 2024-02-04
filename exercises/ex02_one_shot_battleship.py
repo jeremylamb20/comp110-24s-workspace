9d4afd8ed7a1eccdaf450c6741a8ce51b295d00e
Jeremy Lamb <jlamb@unc.edu>
2024-02-04 11:40:49 -0500

    Battleship ex 2 completed

---
+++ exercises/ex02_one_shot_battleship.py
@@ -0,0 +1,48 @@
+"""EX02 - One Shot Battleship"""
+
+__author__ = "730461078"
+
+# Box colors
+BLUE_BOX = "\U0001F7E6"
+RED_BOX = "\U0001F7E5"
+WHITE_BOX = "\U00002B1C"
+
+# Establish variables and grid parameters
+grid_size = 4
+secret_row = 3
+secret_column = 2
+
+# Code for user guesses
+guess_row = int(input("Guess a row: "))
+guess_column = int(input("Guess a column: "))
+
+# Check the user's guess and construct the visual representation of the grid
+row_count = 1
+while row_count <= grid_size:
+    column_count = 1
+    row_string = ""
+    while column_count <= grid_size:
+        if guess_row == row_count:
+            if guess_column == column_count:
+                # Construct the result box (red or white) based on guess
+                if guess_row == secret_row and guess_column == secret_column:
+                    row_string += RED_BOX  # Red box = hit
+                else:
+                    row_string += WHITE_BOX  # White box = miss
+            else:
+                row_string += BLUE_BOX  # Blue box for all other guess options
+        else:
+            row_string += BLUE_BOX  # Blue box for other guess options
+        column_count += 1
+    print(row_string)
+    row_count += 1
+
+# Output of result
+if guess_row == secret_row and guess_column == secret_column:
+    print("Hit!")
+elif guess_row == secret_row:
+    print("Close! Correct column, wrong row.")
+elif guess_column == secret_column:
+    print("Close! Correct row, wrong column.")
+else:
+    print("Miss!")
